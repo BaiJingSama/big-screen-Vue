db96c9fcfe8ddd58066c1a399ea224db5a380e1d
Jackeybai <1303802862@qq.com>
2023-01-13 16:09:22 +0800

    添加计算px的公共scss函数

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
-    你好
+    <div class="x">你好</div>
   </div>
 </template>
 
 <script>
 export default {
-  
+
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+@import '../assets/style/helper.scss';
 
+.x {
+  width: px(961);
+  height: px(91);
+  border: 1px solid red;
+}
 </style>
