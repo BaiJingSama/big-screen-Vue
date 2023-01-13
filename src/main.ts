dd7af0473c014d72f981ca1793cbf9e8403021b9
Jackeybai <1303802862@qq.com>
2023-01-13 16:03:41 +0800

    修复设置可视化部分16:9的bug

--- src/main.ts
+++ src/main.ts
@@ -9,22 +9,7 @@ declare global {
   }
 }
 
-const clientWidth = document.documentElement.clientWidth;
-const clientHeight = document.documentElement.clientHeight;
-window.pageWidth =
-  clientWidth / clientHeight > 16 / 9 ? clientHeight * (16 / 9) : clientWidth;
-const pageHeight = window.pageWidth / (16 / 9);
 
-const string = `<style>html{
-        font-size: ${window.pageWidth / 100}px 
-      }</style>`;
-document.write(string);
-
-const app = <HTMLElement>document.querySelector("#app");
-console.log(app);
-app.style.height = pageHeight + "px";
-app.style.marginTop = (clientHeight - pageHeight) / 2 + "px";
-app.style.border = "1px solid red";
 
 // root.style.width = pageWidth + "px";
 
